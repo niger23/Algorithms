68f65b298eccc91fadac3c57aad8f774e32f9067
Fingerlink <niger-23@yandex.ru>
2023-05-01 12:39:53 +0700

    added new

--- lesson2/HeapSort.java
+++ lesson2/HeapSort.java
@@ -1,3 +1,4 @@
+package lesson2;
 import java.util.Arrays;
 
 public class HeapSort {
---
+++ lesson3/List.java
@@ -0,0 +1,101 @@
+package lesson3;
+
+public class List {
+    Node head;
+    Node tail;
+
+    public void add(int value) {
+        Node node = new Node(); 
+        node.value = value;
+        if (head = null){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+    }
+
+    public void add(int value, Node node) {
+        Node next = node.next;
+        Node newNode = new Node();
+        newNode.value = value;
+        node.next = newNode;
+        newNode.previous = node;
+        if (next == null){
+            tail = newNode;
+        }else{
+            next.previous = newNode;
+            newNode.next = next;
+        }
+    }
+
+    public void delete(Node node) {
+        Node previous = node.previous;
+        Node next = node.next;
+        if (previous == null){
+            next.previous = null;
+            head = next;
+        }else{
+            if(next == null){
+                previous.next = null;
+                tail = previous;
+            } else {
+                previous.next = next;
+                next.previous = previous;
+            }
+        }
+    }
+
+    public Node find(int value){
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == value){
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    public void revert() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node next = currentNode.next;
+            Node previuos = currentNode.previous;
+            currentNode.next = previuos;
+            currentNode.previous = next;
+            if (previuos == null) {
+                tail = currentNode;
+            }
+            if (next == null) {
+                head = currentNode;
+            }
+            currentNode = next;
+        }
+    }
+    public void revert1(){
+        if (head != null && head.next !=null){
+            revert1net(head.next, head);
+        }
+
+    }
+
+    private void revert1net(Node currentNode, Node previousNode) {
+        if (currentNode.next == null) {
+            head = currentNode;
+        } else {
+            revert1net(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+        previousNode.next = null;
+    }
+
+    public class Node{
+        int value;
+
+        Node next;
+        Node previous;
+    }
+}
